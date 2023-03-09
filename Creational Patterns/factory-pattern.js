383f843c6be98b4c045a2ca29b9c34aa16fdc8c3
thebozturk <bugrahanoz061@gmail.com>
2023-03-10 00:24:25 +0300

    refactor: lint names

---
+++ Creational Patterns/factory-pattern.js
@@ -0,0 +1,52 @@
+/* Factory Patterni gerçek dünyadaki gibi nesne üreten bir yer olarak hayal edebiliriz.
+  Örneğin bir fabrika nesne üretir ve bu fabrika bir nesne üretirken nesnenin özelliklerini belirler.
+  Bu özelliklerin belirlenmesi için de bir yapıya ihtiyaç vardır ve Bu yapıyı da fabrika oluşturur
+  Bu yöntem kodun okunabilirliğini arttırır ve kodun tekrar kullanılabilirliğini arttırır.*/
+
+/* Örnek olarak bir yazılım şirketimiz olduğunu varsayalım ve iki çeşit çalışanımızın olduğunu düşünelim.
+    Bunlar yazılımcı ve yöneticiler olmak üzere ikiye ayrılıyor.
+    Bu iki çalışanı türünü veritabanımıza eklenmesini temiz bir şekilde yapılmasını istiyoruz. */
+
+// Öncelikle yazılımcı türünde bir çalışan oluşturan fonksiyonu yazalım.
+function Developer(name) {
+  this.name = name;
+  this.type = "Developer";
+}
+
+// Yönetici türünde bir çalışan oluşturan fonksiyonu yazalım.
+function Manager(name) {
+  this.name = name;
+  this.type = "Manager";
+}
+
+// Şimdi de bu iki çalışanın türünü belirleyen bir fabrika oluşturalım.
+function EmployeeFactory() {
+  this.create = (name, type) => {
+    switch (type) {
+      case 1:
+        return new Developer(name);
+      case 2:
+        return new Manager(name);
+    }
+  };
+}
+
+// Çalışanlarımızı görmek için bir fonksiyon yazalım.
+function say() {
+  console.log(`${this.name} is a ${this.type}`);
+}
+
+// Şimdi de bu fabrikayı kullanarak çalışanlarımızı oluşturalım.
+const employeeFactory = new EmployeeFactory();
+
+// Veritabanımızdaki gibi bir array oluşturalım.
+const employees = [];
+
+// Şimdi de bu arraye çalışanlarımızı ekleyelim.
+employees.push(employeeFactory.create("Buğrahan", 1));
+employees.push(employeeFactory.create("John", 2));
+
+// Son olarak da bu çalışanlarımızı bir döngü ile ekrana yazdıralım.
+employees.forEach((emp) => {
+  say.call(emp);
+});
--- Creational Patterns/singleton-pattern.js
+++ Creational Patterns/singleton-pattern.js
@@ -1,52 +1,25 @@
-/* Factory Patterni gerçek dünyadaki gibi nesne üreten bir yer olarak hayal edebiliriz.
-  Örneğin bir fabrika nesne üretir ve bu fabrika bir nesne üretirken nesnenin özelliklerini belirler.
-  Bu özelliklerin belirlenmesi için de bir yapıya ihtiyaç vardır ve Bu yapıyı da fabrika oluşturur
-  Bu yöntem kodun okunabilirliğini arttırır ve kodun tekrar kullanılabilirliğini arttırır.*/
+/* Singleton Pattern sadece bir tane instance oluşturulmasını sağlar. Ve bu sayede sınırlandırma yapar.
+    Örneğin bir veritabanı bağlantısı oluşturulurken bu bağlantıyı sadece bir tane oluşturmak istiyoruz.
+    Bu bağlantıyı oluşturmak için bir yapıya ihtiyaç vardır ve bu yapıyı da singleton oluşturur.
+    Bu yöntem kodun okunabilirliğini arttırır ve kodun tekrar kullanılabilirliğini arttırır. */
 
-/* Örnek olarak bir yazılım şirketimiz olduğunu varsayalım ve iki çeşit çalışanımızın olduğunu düşünelim.
-    Bunlar yazılımcı ve yöneticiler olmak üzere ikiye ayrılıyor.
-    Bu iki çalışanı türünü veritabanımıza eklenmesini temiz bir şekilde yapılmasını istiyoruz. */
+// Örnek olarak veritabanı bağlantımız olduğunu varsayalım ve bu bağlantıyı sadece bir tane oluşturmak istiyoruz.
 
-// Öncelikle yazılımcı türünde bir çalışan oluşturan fonksiyonu yazalım.
-function Developer(name) {
-  this.name = name;
-  this.type = "Developer";
-}
-
-// Yönetici türünde bir çalışan oluşturan fonksiyonu yazalım.
-function Manager(name) {
-  this.name = name;
-  this.type = "Manager";
-}
-
-// Şimdi de bu iki çalışanın türünü belirleyen bir fabrika oluşturalım.
-function EmployeeFactory() {
-  this.create = (name, type) => {
-    switch (type) {
-      case 1:
-        return new Developer(name);
-      case 2:
-        return new Manager(name);
-    }
+// Öncelikle veritabanı bağlantımızı oluşturan fonksiyonu yazalım.
+function Database() {
+  // Bu fonksiyonu sadece bir tane oluşturmak istiyoruz.
+  if (Database.instance) {
+    return Database.instance;
+  }
+  Database.instance = this;
+  // Veritabanı bağlantımızı simüle edelim.
+  this.connect = () => {
+    console.log("Veritabanı bağlantısı başarılı");
   };
 }
 
-// Çalışanlarımızı görmek için bir fonksiyon yazalım.
-function say() {
-  console.log(`${this.name} is a ${this.type}`);
-}
+// Şimdi de bu bağlantıyı oluşturan bir yapı oluşturalım.
+const db = new Database();
 
-// Şimdi de bu fabrikayı kullanarak çalışanlarımızı oluşturalım.
-const employeeFactory = new EmployeeFactory();
-
-// Veritabanımızdaki gibi bir array oluşturalım.
-const employees = [];
-
-// Şimdi de bu arraye çalışanlarımızı ekleyelim.
-employees.push(employeeFactory.create("Buğrahan", 1));
-employees.push(employeeFactory.create("John", 2));
-
-// Son olarak da bu çalışanlarımızı bir döngü ile ekrana yazdıralım.
-employees.forEach((emp) => {
-  say.call(emp);
-});
+// Bağlantıyı kullanarak bir veritabanı bağlantısı oluşturalım.
+db.connect();
